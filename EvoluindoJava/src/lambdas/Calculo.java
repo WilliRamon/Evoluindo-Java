d035d2a2863371220250246b597032f3108a12cc
WilliRamon <acdc.ramon@hotmail.com>
2022-08-10 01:05:28 -0300

    Estudo Interfaces Funcionais

--- EvoluindoJava/src/lambdas/Calculo.java
+++ EvoluindoJava/src/lambdas/Calculo.java
@@ -1,8 +1,15 @@
 package lambdas;
 
+@FunctionalInterface
 public interface Calculo {
 
 	public abstract double executar(double a, double b); //Não preciso declarar esse método com public e abstract
 
-
+	default String legal() {
+		return "Legal";
+	}
+	
+	static String muitoLegal() {
+		return "muito legal";
+	}
 }
