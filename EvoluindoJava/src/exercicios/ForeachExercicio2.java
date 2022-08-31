f94ad80314877e313fb67329a12356e7819dd3df
WilliRamon <acdc.ramon@hotmail.com>
2022-08-31 02:24:59 -0300

    Update ForeachExercicio2.java

--- EvoluindoJava/src/exercicios/ForeachExercicio2.java
+++ EvoluindoJava/src/exercicios/ForeachExercicio2.java
@@ -1,10 +1,22 @@
 package exercicios;
 
+import java.util.ArrayList;
+
 public class ForeachExercicio2 {
 	
 	public static void main(String[] args) {
 		
+		ArrayList<Integer> notas = new ArrayList<>();
+		notas.add(10);
+		notas.add(6);
+		notas.add(4);
+		notas.add(9);
 		
+		notas.forEach(nota -> {
+			System.out.println(nota);
+		});
+				
+
 	}
 
 }
