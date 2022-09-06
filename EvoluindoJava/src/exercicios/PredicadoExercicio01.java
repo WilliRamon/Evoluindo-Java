f948ab8c00752ed7d41fb526a33e784451ce9a52
WilliRamon <acdc.ramon@hotmail.com>
2022-09-06 01:00:22 -0300

    Interfaces Funcionais - Predicate

---
+++ EvoluindoJava/src/exercicios/PredicadoExercicio01.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.function.Predicate;
+
+import lambdas.Produto;
+
+public class PredicadoExercicio01 {
+	
+	public static void main(String[] args) {
+		
+		Predicate<Produto> isNovo = item -> item.
+		
+		Produto produto = new Produto("Notebook", 3893.89, 0.85);
+		
+		
+	}
+
+}
---
+++ EvoluindoJava/src/lambdas/Predicado.java
@@ -0,0 +1,21 @@
+package lambdas;
+
+import java.util.function.Predicate;
+
+public class Predicado {
+
+	public static void main(String[] args) {
+		
+		Predicate<Produto> isCaro = 
+				prod -> prod.preco >= 750;
+		
+		Produto produto = new Produto("Notebook", 3893.89, 0.85);
+		System.out.println(isCaro.test(produto));
+		
+		
+		Predicate<Produto> isCaro2 =
+				prod -> (prod.preco * (1 - prod.desconto)) >= 750;
+				
+		System.out.println(isCaro2.test(produto));
+	}
+}
