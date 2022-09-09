41e2b5d98d76bae152026bf976d572479fee41e8
WilliRamon <acdc.ramon@hotmail.com>
2022-09-09 01:36:47 -0300

    Create OperadorBinario.java

---
+++ EvoluindoJava/src/lambdas/OperadorBinario.java
@@ -0,0 +1,26 @@
+package lambdas;
+
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class OperadorBinario {
+	
+	public static void main(String[] args) {
+		
+		BinaryOperator<Double> media = (num1, num2) -> (num1 + num2) / 2;
+		System.out.println(media.apply(9.8, 5.7));
+		
+		BiFunction<Double, Double, String> resultado = (n1, n2) -> {
+			double notaFinal = (n1 + n2) / 2;
+			return notaFinal >= 7 ? "Aprovado" : "Reprovado";
+		};	
+		
+		System.out.println(resultado.apply(10.0, 0.0));
+		
+		Function<Double, String> conceito = m -> m >= 7 ? "Aprovado" : "Reprovado";
+		
+		System.out.println(media.andThen(conceito).apply(2.5, 5.7));
+	}
+
+}
