a7eed052f85ed7679caf8ff1030c5baa43028301
WilliRamon <acdc.ramon@hotmail.com>
2022-10-25 00:20:43 -0300

    Create DesafioMap01.java

---
+++ EvoluindoJava/src/streams/DesafioMap01.java
@@ -0,0 +1,16 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DesafioMap01 {
+	
+	/*
+	 * 1. Número para string binária... 6 => "110"
+	 * 2. Inverter a string... "110" => "011"
+	 * 3. Converter de volta para inteiro => "011" => 3
+	 */
+	
+	List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+}
