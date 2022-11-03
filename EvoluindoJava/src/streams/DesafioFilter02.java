be3c1c123114e3048586ed22b8994446f99c76f8
WilliRamon <acdc.ramon@hotmail.com>
2022-11-03 01:04:47 -0300

    Inicio segundo desafio Finter

---
+++ EvoluindoJava/src/streams/DesafioFilter02.java
@@ -0,0 +1,11 @@
+package streams;
+
+public class DesafioFilter02 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
---
+++ EvoluindoJava/src/streams/Produto.java
@@ -0,0 +1,10 @@
+package streams;
+
+public class Produto {
+	
+//	nome
+//	preco
+//	desconto
+//	frete
+
+}
