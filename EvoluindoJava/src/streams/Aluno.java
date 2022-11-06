906047735eee906f65d249b74c45e48333cf0b38
WilliRamon <acdc.ramon@hotmail.com>
2022-11-06 04:10:57 -0300

    Segundo exercício de Reduce

--- EvoluindoJava/src/streams/Aluno.java
+++ EvoluindoJava/src/streams/Aluno.java
@@ -4,9 +4,11 @@ public class Aluno {
 	
 	final String nome;
 	final double nota;
+	final boolean bomComportamento;
 	
 	public Aluno(String nome, double nota) {
 		this.nome = nome;
 		this.nota = nota;
+		this.bomComportamento = true;
 	}
 }
