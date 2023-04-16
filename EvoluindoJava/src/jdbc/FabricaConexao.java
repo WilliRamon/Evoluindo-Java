629c1b66469dfd7b75898d049240fbbf66d07d51
WilliRamon <acdc.ramon@hotmail.com>
2023-04-16 01:59:24 -0300

    Arquivo .properties

--- EvoluindoJava/src/jdbc/FabricaConexao.java
+++ EvoluindoJava/src/jdbc/FabricaConexao.java
@@ -1,23 +1,37 @@
 package jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 	
 	public static Connection getConexao() {
 		
 		try {
-			final String url = "jdbc:mysql://localhost/db_curso_java?verifyServerCertificate=false&useSSL=true";
-			final String usuario = "root";
-			final String senha = "oluapoas5801";
+			Properties prop = getProperties();
+			final String url = prop.getProperty("banco.url");
+			final String usuario = prop.getProperty("banco.usuario");
+			final String senha = prop.getProperty("banco.senha");		
+			
+//			final String url = "jdbc:mysql://localhost/db_curso_java?verifyServerCertificate=false&useSSL=true";
+//			final String usuario = "root";
+//			final String senha = "oluapoas5801";
 
 			return DriverManager.getConnection(url, usuario, senha);
 
-		} catch (SQLException e) {
+		} catch (SQLException | IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	private static Properties getProperties() throws IOException {
+		Properties prop = new Properties();
+		String caminho = "/conexao.properties";
+		prop.load(FabricaConexao.class.getResourceAsStream(caminho));
+		return prop;
 	}
 
 }
