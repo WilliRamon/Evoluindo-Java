b743647ac9496a26273fbacae89f3598212e2204
WilliRamon <acdc.ramon@hotmail.com>
2023-04-17 01:09:34 -0300

    Inicio dos estudos de JPA

--- EvoluindoJava/src/jdbc/DAOTeste.java
+++ EvoluindoJava/src/jdbc/DAOTeste.java
@@ -11,5 +11,7 @@ public class DAOTeste {
 		System.out.println(dao.incluir(sql, "João da Silva"));
 		System.out.println(dao.incluir(sql, "Ana Julia"));
 		System.out.println(dao.incluir(sql, "Djalma Pereira"));
+		
+		dao.close();
 	}
 }
