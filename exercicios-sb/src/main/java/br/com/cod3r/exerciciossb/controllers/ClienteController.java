6426c2e2b3e0993f6cb547d8bf9e76cfbb0c7188
WilliRamon <acdc.ramon@hotmail.com>
2023-08-02 00:33:49 -0300

    Passando parametros por URL

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -1,17 +1,23 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exerciciossb.models.Cliente;
 
 @RestController
-@RequestMapping("clientes")
+@RequestMapping("/clientes")
 public class ClienteController {
 
 	@GetMapping("/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(28, "Willi", "123.456.789-00");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "123.456.789-00");
 	}
 }
