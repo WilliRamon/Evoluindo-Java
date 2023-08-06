af1056fa91e0c8ecb230bddcab665bdb4aabffd2
WilliRamon <acdc.ramon@hotmail.com>
2023-08-06 04:31:32 -0300

    Persistindo via requisição POST

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/entities/Produto.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/entities/Produto.java
@@ -2,6 +2,8 @@ package br.com.cod3r.exerciciossb.model.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,8 +12,32 @@ import javax.persistence.Table;
 public class Produto {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
 	@Column(name = "nome_produto", length = 200, nullable = false)
 	private String nome;
+	
+	public Produto() {
+	
+	}
+
+	public Produto(String nome) {
+		this.nome = nome;
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
