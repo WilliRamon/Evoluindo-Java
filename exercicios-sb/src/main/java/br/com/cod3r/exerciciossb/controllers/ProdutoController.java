3c8541d6e1460dcdf4fce4446db0fb48cc9c513a
WilliRamon <acdc.ramon@hotmail.com>
2023-08-24 00:44:21 -0300

    Deletando um registro

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -9,6 +9,7 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -93,5 +94,18 @@ public class ProdutoController {
 	public Produto salvarProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void exluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+	}
+	
+	@DeleteMapping("/generation/{id}")
+	public ResponseEntity<?> exluirProdutoGen(@PathVariable int id) {
+		return produtoRepository.findById(id).map(resposta -> {
+			produtoRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}).orElse(ResponseEntity.notFound().build());
 	}
 }
