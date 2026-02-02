e4c5e54f3f09aca5f2ac1343e425062f6e02fca4
Willi Ramon <82010630+WilliRamon@users.noreply.github.com>
2026-02-01 23:57:14 -0300

    Usuario Controller

---
+++ recap - fiap/usuarios_pettech/src/main/java/br/com/fiap/api/usuarios_pettech/controller/UsuarioController.java
@@ -0,0 +1,46 @@
+package br.com.fiap.api.usuarios_pettech.controller;
+
+import br.com.fiap.api.usuarios_pettech.dto.UsuarioDTO;
+import br.com.fiap.api.usuarios_pettech.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @Autowired
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UsuarioDTO>> findAll(@PageableDefault(size = 10, page = 0, sort = "nome") Pageable pageable) {
+        return ResponseEntity.ok(usuarioService.findAll(pageable));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(usuarioService.findById(id));
+    }
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> save(@RequestBody UsuarioDTO usuarioDTO) {
+        return new ResponseEntity<>(usuarioService.save(usuarioDTO), HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> update(@PathVariable Long id, @RequestBody UsuarioDTO usuarioDTO) {
+        return new ResponseEntity<>(usuarioService.update(id, usuarioDTO), HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        usuarioService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
