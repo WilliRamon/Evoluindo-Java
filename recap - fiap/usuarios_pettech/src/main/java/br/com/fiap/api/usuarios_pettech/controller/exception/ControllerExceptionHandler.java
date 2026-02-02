e878a8bf85e9afe3a37b8e54aaa0339e5417b71d
Willi Ramon <82010630+WilliRamon@users.noreply.github.com>
2026-02-02 01:09:45 -0300

    Implementando ValidateError

--- recap - fiap/usuarios_pettech/src/main/java/br/com/fiap/api/usuarios_pettech/controller/exception/ControllerExceptionHandler.java
+++ recap - fiap/usuarios_pettech/src/main/java/br/com/fiap/api/usuarios_pettech/controller/exception/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package br.com.fiap.api.usuarios_pettech.controller.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -28,11 +29,17 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
-        err.setTimestamp(Instant.now());
-        err.setStatus(status.value());
-        err.setError("Erro de validacao");
-        err.setMessage(e.getMessage());
-        err.setPath(request.getRequestURI());
-        return ResponseEntity.status(status).body(err);
+        ValidateError validateError = new ValidateError();
+        validateError.setTimestamp(Instant.now());
+        validateError.setStatus(status.value());
+        validateError.setError("Erro de validacao");
+        validateError.setMessage(e.getMessage());
+        validateError.setPath(request.getRequestURI());
+
+        for(FieldError f : e.getBindingResult().getFieldErrors()) {
+            validateError.addMensagens(f.getField(), f.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(validateError);
     }
 }
--- recap - fiap/usuarios_pettech/src/main/java/br/com/fiap/api/usuarios_pettech/dto/UsuarioDTO.java
+++ recap - fiap/usuarios_pettech/src/main/java/br/com/fiap/api/usuarios_pettech/dto/UsuarioDTO.java
@@ -1,5 +1,6 @@
 package br.com.fiap.api.usuarios_pettech.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
@@ -8,6 +9,7 @@ public record UsuarioDTO(
         Long id,
         @NotBlank(message = "O nome não pode estar em branco.")
         String nome,
+        @Email(message = "E-mail invalido.")
         String email,
         String cpf,
         LocalDate dataNascimento
