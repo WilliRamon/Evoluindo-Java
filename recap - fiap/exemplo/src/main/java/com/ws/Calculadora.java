56515076c0568e46332de95a3720650476a41ef9
Willi Ramon <82010630+WilliRamon@users.noreply.github.com>
2026-05-29 02:03:13 -0300

    Testes Unitários - Calculadora

--- recap - fiap/exemplo/src/main/java/com/ws/Calculadora.java
+++ recap - fiap/exemplo/src/main/java/com/ws/Calculadora.java
@@ -5,5 +5,13 @@ public class Calculadora {
     public int somar(int variavelA, int variavelB){
         return variavelA + variavelB;
     }
-
+    public int subtrair(int variavelA, int variavelB){
+        return variavelA - variavelB;
+    }
+    public int multiplicar(int variavelA, int variavelB){
+        return variavelA * variavelB;
+    }
+    public int dividir(int variavelA, int variavelB){
+        return variavelA / variavelB;
+    }
 }
--- recap - fiap/exemplo/src/test/java/CalculadoraTest.java
+++ recap - fiap/exemplo/src/test/java/CalculadoraTest.java
@@ -1,28 +1,38 @@
 import com.ws.Calculadora;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 class CalculadoraTest {
 
+    private Calculadora calculadora;
+
+    @BeforeEach
+    void setup(){
+        calculadora = new Calculadora();
+    }
+
     @Test
     void deveSomar() {
-        Calculadora calculadora = new Calculadora();
         int resultado = calculadora.somar(4, 2);
         Assertions.assertEquals(6, resultado);
     }
 
     @Test
     void deveSubtrair() {
-        Assertions.fail("Teste não implementado");
+        int resultado = calculadora.subtrair(4, 2);
+        Assertions.assertEquals(2, resultado);
     }
 
     @Test
     void deveMultiplicar() {
-        Assertions.fail("Teste não implementado");
+        int resultado = calculadora.multiplicar(4, 2);
+        Assertions.assertEquals(8, resultado);
     }
 
     @Test
     void deveDividir() {
-        Assertions.fail("Teste não implementado");
+        int resultado = calculadora.dividir(4, 2);
+        Assertions.assertEquals(2, resultado);
     }
 }
