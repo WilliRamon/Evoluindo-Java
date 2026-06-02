f337fa65ad8b4ba871ff140c15f9d42d4c936a9a
Willi Ramon <82010630+WilliRamon@users.noreply.github.com>
2026-06-02 01:53:34 -0300

    Qualidade de Software - Projeto com JUnit - Implementando primeiras camadas de testes e configurando o pom

--- recap - fiap/backend_aula_junit/src/test/java/br/com/fiap/api/repository/MensagemRepositoryTest.java
+++ recap - fiap/backend_aula_junit/src/test/java/br/com/fiap/api/repository/MensagemRepositoryTest.java
@@ -1,4 +1,28 @@
 package br.com.fiap.api.repository;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class MensagemRepositoryTest {
+
+    @Test
+    void devePermitirRegistrarMensagem() {
+        fail("Teste não implementado");
+    }
+
+    @Test
+    void devePermitirBuscarMensagem() {
+        fail("Teste não implementado");
+    }
+
+    @Test
+    void devePermitirAlterarMensagem() {
+        fail("Teste não implementado");
+    }
+
+    @Test
+    void devePermitirRemoverMensagem() {
+        fail("Teste não implementado");
+    }
 }
